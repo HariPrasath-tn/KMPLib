c66d55c13248f7ca5512452e7cdac22987fc7afd
Hari Prasath <87307438+HariPrasath-tn@users.noreply.github.com>
2025-11-05 10:53:59 +0530

    -

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "dataKit",
-         url: "https://files-accl.zohoexternal.com/public/workdrive-external/download/coy6013fe1e4c352d47189b88babef522bbb3?x-cli-msg=%7B%22linkId%22%3A%225JDJr6e3nea-M5mwF%22%2C%22isFileOwner%22%3Afalse%2C%22version%22%3A%221.0%22%2C%22isWDSupport%22%3Afalse%7D",
+         url: "https://github.com/HariPrasath-tn/KMPLib/releases/download/1.0.0/dataKit.xcframework.zip",
          checksum: "63759643abb9dd39af961d3ac4534fbf4aa74c33654e4a108ee2e7caacaa8728")
    ]
 )
